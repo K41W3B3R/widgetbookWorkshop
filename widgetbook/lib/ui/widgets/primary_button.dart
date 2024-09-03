bcf27f51e4df0c5d1cd256783a592dbe568c5fe6
Kai Weber <kai.weber@tchibo.de>
2024-09-03 16:39:36 +0200

    init

--- widgetbook/lib/ui/widgets/primary_button.dart
+++ widgetbook/lib/ui/widgets/primary_button.dart
@@ -5,5 +5,11 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:groceries_app/ui/ui.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 /// Add Widgetbook use-case here
+@widgetbook.UseCase(name: 'Default', type: PrimaryButton)
+Widget buildPrimaryButtonUseCase(BuildContext context) {
+  return PrimaryButton(content: context.knobs.string(label: 'content', initialValue: 'Widget Workshop'));
+
+}
